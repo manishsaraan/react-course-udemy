ce93995cd377301d7f7edfb5f4c0da5db76f36f4
manishsaraan <manish021js@gmail.com>
2019-05-02 22:18:36 +0530

    Using prop types in react

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 import classes from "./Person.css";
 import withClass from "../../../hoc/withClass";
 import Aux from "../../../hoc/Aux";
@@ -21,5 +21,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  changed: PropTypes.func.isRequired
+};
 
 export default withClass(Person, classes.Person);
