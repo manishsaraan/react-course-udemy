6733dfe82ffa25be085a39a26660dd3c8c47d336
manishsaraan <manish021js@gmail.com>
2019-05-07 22:11:43 +0530

    Creating inital structure

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "./components/Layouts/Layout";
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Layout>
+        <p>test</p>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
