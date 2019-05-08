dd0d89a8abee75d066a87b4e4af343bcd3d9d367
manishsaraan <manish021js@gmail.com>
2019-05-08 22:08:06 +0530

    Ingredient componet created

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "./components/Layouts/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>test</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
