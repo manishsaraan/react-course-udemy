d8063ee42ad4512de304e065bbeedf5322e19d4f
manishsaraan <manish021js@gmail.com>
2019-05-12 13:06:12 +0530

    Button component created

--- burger-project/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-project/src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,8 @@ const orderSummary = props => {
       <p>Delicious burger with following ingredients</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   );
 };
