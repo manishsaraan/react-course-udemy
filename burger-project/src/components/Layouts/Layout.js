809104c794a1945097e30ae6a5c3c62648962bc9
manishsaraan <manish021js@gmail.com>
2019-05-13 13:29:04 +0530

    Toolbar component created

--- burger-project/src/components/Layouts/Layout.js
+++ burger-project/src/components/Layouts/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import Toolbar from "../UI/Navigation/Toolbar/Toolbar";
 import classes from "./Layout.css";
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
