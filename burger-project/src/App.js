96cfeea79b598a72ad1810a019d8e0dac1300361
manishsaraan <manish021js@gmail.com>
2019-07-01 21:55:12 +0530

    Minor folder structure changes

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./components/Layouts/Layout";
+import Layout from "./hoc/Layouts/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
--- burger-project/src/hoc/Layouts/Layout.js
+++ burger-project/src/hoc/Layouts/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
-import Toolbar from "../UI/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../UI/Navigation/SideDrawer/SideDrawer";
+import Aux from "../Aux/Aux";
+import Toolbar from "../../components/UI/Navigation/Toolbar/Toolbar"; // "../../UI/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/UI/Navigation/SideDrawer/SideDrawer";
 import classes from "./Layout.css";
 
 class Layout extends Component {
