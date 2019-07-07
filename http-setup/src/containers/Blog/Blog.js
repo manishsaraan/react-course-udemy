8717efb982c2ae0d024595250411646c1d512d1a
manishsaraan <manish021js@gmail.com>
2019-07-07 15:33:04 +0530

    Ussing https to get posts from fake server

---
+++ http-setup/src/containers/Blog/Blog.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+
+class Blog extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    const posts = axios("https://jsonplaceholder.typicode.com/posts").then(
+      response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => ({ ...post, author: "max" }));
+        this.setState({ posts: updatedPosts });
+      }
+    );
+  }
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} author={post.author} title={post.title} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
