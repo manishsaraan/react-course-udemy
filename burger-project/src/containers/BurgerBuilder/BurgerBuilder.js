4257fb3d78312d9cf12846f685b029e602d63b57
manishsaraan <manish021js@gmail.com>
2019-07-16 15:19:18 +0530

    Saving orders in db

--- burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-order";
 
 const INGREDIENT_PRICE = {
   salad: 0.5,
@@ -82,7 +83,26 @@ class BurgerBuild extends Component {
     this.setState({ purchasing: false });
   };
 
-  purchaseContinueHandler = () => alert("You continue!!!");
+  purchaseContinueHandler = () => {
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Manish Saraan",
+        address: {
+          street: "Teststreet 1",
+          zipCode: 123454,
+          country: "India"
+        },
+        email: "test@test.com",
+        deliveryMethod: "fastest"
+      }
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
 
   render() {
     const disabledInfo = {
