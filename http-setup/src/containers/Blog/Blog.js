74de58ad6609dac8f41d58dc392fbac8f0a512fe
manishsaraan <manish021js@gmail.com>
2019-07-25 18:19:36 +0530

    Basic routing added

--- http-setup/src/containers/Blog/Blog.js
+++ http-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -18,7 +20,10 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/"  render={() => <h1>Home 2</h1>} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact exact component={NewPost} />
       </div>
     );
   }
