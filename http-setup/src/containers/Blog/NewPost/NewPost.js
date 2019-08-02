1a9657c102172ac22797d64ef2dac71546fff716
manishsaraan <manish021js@gmail.com>
2019-08-01 14:21:17 -1000

    redirecting routes programatically

--- http-setup/src/containers/Blog/NewPost/NewPost.js
+++ http-setup/src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
 import "./NewPost.css";
 
 class NewPost extends Component {
   state = {
     title: "",
     content: "",
-    author: "Max"
+    author: "Max",
+    submitted: false
   };
 
   componentDidMount() {
@@ -22,12 +24,15 @@ class NewPost extends Component {
       .post("https://jsonplaceholder.typeicode.com/posts", post)
       .then(response => {
         console.log(response);
+        this.props.history.push("/posts");
+        // this.setState({ submitted: true });
       });
   };
 
   render() {
     return (
       <div className="NewPost">
+        {this.state.submitted && <Redirect to="/posts" />}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
