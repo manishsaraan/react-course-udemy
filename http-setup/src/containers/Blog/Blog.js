244cb63ea5847b21712dcfc7ad065d084dae8699
manishsaraan <manish021js@gmail.com>
2019-08-02 14:50:23 -1000

    Lazy loading a component with HOC

--- http-setup/src/containers/Blog/Blog.js
+++ http-setup/src/containers/Blog/Blog.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
+
+//import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent";
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   };
 
   render() {
@@ -46,7 +51,7 @@ class Blog extends Component {
         <Route path="/"  render={() => <h1>Home 2</h1>} /> */}
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" component={NewPost} />
+            <Route path="/new-post" component={AsyncNewPost} />
           ) : null}
           <Route path="/posts" component={Posts} />
           {/* <Route path="/" component={Posts} /> */}
