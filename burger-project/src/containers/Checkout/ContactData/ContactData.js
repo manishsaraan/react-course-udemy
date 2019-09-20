b2612b9c66d9f86eebdb46d8a69b9b906232fcb2
manishsaraan <manish021js@gmail.com>
2019-09-20 13:18:37 +0530

    Submitting customer data from form

--- burger-project/src/containers/Checkout/ContactData/ContactData.js
+++ burger-project/src/containers/Checkout/ContactData/ContactData.js
@@ -65,16 +65,22 @@ class ContactData extends React.Component {
 
   orderHandler = event => {
     event.preventDefault();
-    console.log(this.props);
+
     this.setState({ loading: true });
+
+    const formData = {};
+    for(let formElementIdentifier in this.state.orderForm){
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
+
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      orderData : formData
     };
     axios
       .post("/orders.json", order)
       .then(response => {
-        console.log(response);
         this.setState({ loading: false });
         this.props.history.push("/");
       })
@@ -119,7 +125,7 @@ class ContactData extends React.Component {
         ))
       }
         
-      <Button btnType="Success">Ordedfdgr</Button>
+      <Button btnType="Success">ORDER</Button>
       </form>
     );
     if (this.state.loading) {
