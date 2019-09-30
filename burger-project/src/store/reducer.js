a4743116ebc4c2ec67eaaa4d0868c5b633ced53c
manishsaraan <manish021js@gmail.com>
2019-09-30 20:32:34 +0530

    updated reducer to handel ingredients

--- burger-project/src/store/reducer.js
+++ burger-project/src/store/reducer.js
@@ -1,17 +1,37 @@
 import * as actionTypes from './actions';
 
 const intialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0, 
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 0
 };
 
 const reducer = (state = intialState, action) => {
     switch(action.type){
         case actionTypes.ADD_INGREDIENTS:
-        return {}
+        return {
+         ...state,
+         ingredients: {
+             ...state.ingredients,
+             [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+         }
+        };
+        break;
+        case actionTypes.REMOVE_INGREDIENTS:
+        return {
+         ...state,    
+         ingredients: {
+            ...state.ingredients,
+            [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }        
+        };
         break;
         default:
-        return {}
+        return {...state}
     }
 }
 
