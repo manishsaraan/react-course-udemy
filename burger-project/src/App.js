e45fe90b766f89a90d7a52b0d8196df6671d66e2
manishsaraan <manish021js@gmail.com>
2019-10-07 21:52:18 +0530

    Protecting the routes with route guards

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import { authCheckStatus } from './store/actions'
 import Layout from "./hoc/Layouts/Layout";
@@ -15,16 +15,28 @@ class App extends React.Component {
   }
 
   render(){
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/"/>
+      </Switch>
+    );
+
+    if(this.props.isAuth){
+      routes = ( <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />            
+        <Route path="/logout" component={Logout} />     
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/"/>
+      </Switch>);
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
@@ -32,6 +44,9 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = state => ({
+    isAuth: state.auth.token !== null
+});
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -39,4 +54,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
